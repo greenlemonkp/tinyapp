3fbdb341a46e6f68bf9119e07366848729710a8d
JOON LEE <110060709+greenlemonkp@users.noreply.github.com>
2022-10-28 12:16:08 -0400

    added second route

--- express_server.js
+++ express_server.js
@@ -4,6 +4,15 @@ const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:id", (req, res) => {
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  res.render("urls_show", templateVars);
+});
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
